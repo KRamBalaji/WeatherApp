743c9200d303e7d997dfd959f1623a421f1af100
rambalaji <rambalajikoppula1@gmail.com>
2022-09-17 22:04:28 +0530

    initialised project

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Ram Balaji Koppula on 17/09/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
