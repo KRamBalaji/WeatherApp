2fbd3621a302a9b73aa0d200eb492eec3ba7576d
rambalaji <rambalajikoppula1@gmail.com>
2022-09-23 22:04:18 +0530

    created WelcomeView to display co-ordinates and LoadingView, which is a custom ProgressView

--- WeatherApp/Views/ContentView.swift
+++ WeatherApp/Views/ContentView.swift
@@ -12,7 +12,23 @@ struct ContentView: View {
     @StateObject var locationManager = LocationManager()
     
     var body: some View {
-        Text("Hello World")
+        VStack {
+            
+            if let location = locationManager.location {
+                Text("Your co-ordinates are \(location.longitude),\(location.latitude)")
+            }
+            else {
+                if locationManager.isLoading {
+                    LoadingView()
+                }
+                else {
+                    WelcomeView()
+                        .environmentObject(locationManager)
+                }
+            }
+        }
+        .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+        .preferredColorScheme(.dark)
     }
 }
 
